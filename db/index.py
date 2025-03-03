5bfd98d5bafa5399a8668a7e324f3f7b1202613c
Joseph Wasden <jpwasden93@gmail.com>
2025-03-03 10:17:34 +0000

    Added index

--- db/index.py
+++ db/index.py
@@ -9,6 +9,7 @@ async def delete_indexes(pool: Pool) -> None:
         await asyncio.gather(
             pool.execute("drop index rule_multi_index"),
             pool.execute("drop index card_index_id_name_date_rule_id"),
+            pool.execute("drop index distinct_cards_ix"),
             return_exceptions=False
         )
     except asyncpg.exceptions.UndefinedObjectError:
@@ -35,8 +36,38 @@ async def add_indexes(pool: Pool) -> None:
                     on card(id, name, normalised_name, release_date, rule_id);
             """
 
+    distinct_cards_ix = """
+                create index distinct_cards_ix
+                    on distinct_cards(front_name,
+                                    front_normalised_name,
+                                    front_scryfall_url,
+                                    front_image_id,
+                                    front_mana_cost,
+                                    front_colour_identity,
+                                    front_power,
+                                    front_toughness,
+                                    front_loyalty,
+                                    front_defence,
+                                    front_type_line,
+                                    front_oracle_text,
+                                    back_name,
+                                    back_scryfall_url,
+                                    back_image_id,
+                                    back_mana_cost,
+                                    back_colour_identity,
+                                    back_power,
+                                    back_toughness,
+                                    back_loyalty,
+                                    back_defence,
+                                    back_type_line,
+                                    back_oracle_text)
+        """
+
     await asyncio.gather(
         pool.execute(rule_index),
         pool.execute(card_index),
+        pool.execute(distinct_cards_ix),
         return_exceptions=False
     )
+
+
